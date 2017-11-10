aabee69308c9918f335c53cbbfcf705e27e40300
pimier15 <pimier15@gmail.com>
2017-11-10 21:14:36 +0900

    update

--- 01Sub/ModelLib/TestConsole/ValPosTest.cs
+++ 01Sub/ModelLib/TestConsole/ValPosTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ModelLib.Data.Handler;
 
 namespace TestConsole
 {
-	class ValPosTest
+	public class ValPosTest
 	{
+		public static void main()
+		{
+			var pos2 = ValPos(1,2,0);
+			var pos3 = ValPos(3,2,0);
+			var res1 = pos2 > pos3;
+			var res2 = pos2 + pos3;
+			var res3 = pos2.ad + pos3.ADDPos;
+		}
 	}
 }
