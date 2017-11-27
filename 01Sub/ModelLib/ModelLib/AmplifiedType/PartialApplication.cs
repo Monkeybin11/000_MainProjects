ae8e822904c61ef0ecf5e3494dcdfbebb047c029
pimier15 <pimier15@gmail.com>
2017-11-27 16:42:51 +0900

    update

---
+++ 01Sub/ModelLib/ModelLib/AmplifiedType/PartialApplication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib.AmplifiedType
+{
+	public static class PartialApplication
+	{
+		//For Function
+		public static Func<T1 , R> Compose<T1, T2, R>( this Func<T2 , R> g , Func<T1 , T2> f )
+			=> x => g( f( x ) );
+
+		public static Func<T2 , R> Apply<T1, T2, R>( this Func<T1 , T2 , R> func , T1 t1 )
+			=> t2 => func( t1 , t2 );
+
+		public static Func<T2 , T3 , R> Apply<T1, T2, T3, R>( this Func<T1 , T2 , T3 , R> func , T1 t1 )
+			=> ( t2 , t3 ) => func( t1 , t2 , t3 );
+
+		public static Func<T2 , T3 , T4 , R> Apply<T1, T2, T3 , T4 , R>( this Func<T1 , T2 , T3 , T4 , R> func , T1 t1 )
+			=> ( t2 , t3  , t4) => func( t1 , t2 , t3 , t4 );
+
+		public static Func<T2 , T3 , T4 , T5 , R> Apply<T1, T2, T3 , T4 , T5 , R>( this Func<T1 , T2 , T3 , T4 , T5  , R> func , T1 t1 )
+			=> ( t2 , t3 , t4 , t5) => func( t1 , t2 , t3 , t4 , t5);
+
+
+		public static Func<I1 , I2 , R> Map<I1, I2, T, R>( this Func<I1 , I2 , T> @this , Func<T , R> func )
+		   => ( i1 , i2 ) => func( @this( i1 , i2 ) );
+
+
+		//For Action
+
+		public static Action<T2 > Apply<T1, T2>( this Action<T1 , T2 > func , T1 t1 )
+			=> t2 => func( t1 , t2 );
+
+		public static Action<T2 , T3 > Apply<T1, T2, T3>( this Action<T1 , T2 , T3 > func , T1 t1 )
+			=> ( t2 , t3 ) => func( t1 , t2 , t3 );
+
+		public static Action<T2 , T3 , T4 > Apply<T1, T2, T3, T4>( this Action<T1 , T2 , T3 , T4 > func , T1 t1 )
+			=> ( t2 , t3 , t4 ) => func( t1 , t2 , t3 , t4 );
+
+		public static Action<T2 , T3 , T4 , T5 > Apply<T1, T2, T3, T4, T5>( this Action<T1 , T2 , T3 , T4 , T5 > func , T1 t1 )
+			=> ( t2 , t3 , t4 , t5 ) => func( t1 , t2 , t3 , t4 , t5 );
+
+
+		public static Action<I1 , I2 > Map<I1, I2, T>( this Func<I1 , I2 , T> @this , Action<T > func )
+		   => ( i1 , i2 ) => func( @this( i1 , i2 ) );
+
+
+
+
+	}
+}
--- 01Sub/ModelLib/ModelLib/Properties/AssemblyInfo.cs
+++ 01Sub/ModelLib/ModelLib/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ModelLib")]
+[assembly: AssemblyDescription( "Add Partial Application for Func , Action" )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ModelLib")]
@@ -31,3 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.3.0" )]
+[assembly: AssemblyFileVersion( "1.0.3.0" )]
