5d8fd51c6c7cfb333c69146d36e8e878f5844fa0
pimier15 <pimier15@gmail.com>
2017-12-15 16:19:50 +0900

    update Indexing , Defect Inspection

--- 01Sub/ModelLib/ModelLib/AmplifiedType/PartialApplication.cs
+++ 01Sub/ModelLib/ModelLib/AmplifiedType/PartialApplication.cs
@@ -21,10 +21,26 @@ namespace ModelLib.AmplifiedType
 		public static Func<T2 , T3 , T4 , R> Apply<T1, T2, T3 , T4 , R>( this Func<T1 , T2 , T3 , T4 , R> func , T1 t1 )
 			=> ( t2 , t3  , t4) => func( t1 , t2 , t3 , t4 );
 
-		public static Func<T2 , T3 , T4 , T5 , R> Apply<T1, T2, T3 , T4 , T5 , R>( this Func<T1 , T2 , T3 , T4 , T5  , R> func , T1 t1 )
-			=> ( t2 , t3 , t4 , t5) => func( t1 , t2 , t3 , t4 , t5);
+		public static Func<T2 , T3 , T4 , T5 , R> Apply<T1, T2, T3, T4, T5, R>( this Func<T1 , T2 , T3 , T4 , T5 , R> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 ) => func( t1 , t2 , t3 , t4 , t5 );
+
+		public static Func<T2 , T3 , T4 , T5 , T6 , R> Apply<T1, T2, T3, T4, T5, T6, R>( this Func<T1 , T2 , T3 , T4 , T5 , T6 , R> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 ) => func( t1 , t2 , t3 , t4 , t5 , t6 );
+
+		public static Func<T2 , T3 , T4 , T5 , T6 , T7 , R> Apply<T1, T2, T3, T4, T5, T6, T7, R>( this Func<T1 , T2 , T3 , T4 , T5 , T6 , T7 , R> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 ) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 );
 
 
+		public static Func<T2 , T3 , T4 , T5 , T6 , T7 , T8 , R> Apply<T1, T2, T3, T4, T5, T6, T7, T8, R>( this Func<T1 , T2 , T3 , T4 , T5 , T6 , T7 , T8 , R> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 , t8 ) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 , t8 );
+
+		public static Func<T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9 , R> Apply<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>( this Func<T1 , T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9 , R> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9 ) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9 );
+
+		public static Func<T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9 , T10 , R> Apply<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>( this Func<T1 , T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9 , T10 , R> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9 , t10 ) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9 , t10 );
+
+		
 		public static Func<I1 , I2 , R> Map<I1, I2, T, R>( this Func<I1 , I2 , T> @this , Func<T , R> func )
 		   => ( i1 , i2 ) => func( @this( i1 , i2 ) );
 
@@ -42,6 +58,22 @@ namespace ModelLib.AmplifiedType
 
 		public static Action<T2 , T3 , T4 , T5 > Apply<T1, T2, T3, T4, T5>( this Action<T1 , T2 , T3 , T4 , T5 > func , T1 t1 )
 			=> ( t2 , t3 , t4 , t5 ) => func( t1 , t2 , t3 , t4 , t5 );
+
+		public static Action<T2 , T3 , T4 , T5 , T6> Apply<T1, T2, T3, T4, T5 , T6>( this Action<T1 , T2 , T3 , T4 , T5 , T6> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 ) => func( t1 , t2 , t3 , t4 , t5 , t6);
+
+		public static Action<T2 , T3 , T4 , T5 , T6 , T7> Apply<T1, T2, T3, T4, T5, T6, T7>( this Action<T1 , T2 , T3 , T4 , T5 , T6 , T7> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 ) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7);
+
+
+		public static Action<T2 , T3 , T4 , T5 , T6 , T7 , T8> Apply<T1, T2, T3, T4, T5, T6, T7, T8>( this Action<T1 , T2 , T3 , T4 , T5 , T6 , T7 , T8> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 , t8) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 , t8 );
+
+		public static Action<T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9> Apply<T1, T2, T3, T4, T5, T6, T7, T8, T9>( this Action<T1 , T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9);
+
+		public static Action<T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9 , T10> Apply<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>( this Action<T1 , T2 , T3 , T4 , T5 , T6 , T7 , T8 , T9 , T10> func , T1 t1 )
+		=> ( t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9 ,t10 ) => func( t1 , t2 , t3 , t4 , t5 , t6 , t7 , t8 , t9  ,t10);
 
 
 		public static Action<I1 , I2 > Map<I1, I2, T>( this Func<I1 , I2 , T> @this , Action<T > func )
